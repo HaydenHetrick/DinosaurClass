77e16b558305db06f56e4655437535f78b65e0af
Hayden Hetrick <hetrickhay@gmail.com>
2024-10-22 12:49:55 -0400

    Add project files.

---
+++ DinosaurClass/Dinosaur.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinosaurClass
+{
+    internal class Dinosaur
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string Diet{ get; set; }
+        public int Age { get; set; }
+
+        public Dinosaur(string name, string species, string diet, int age)
+        {
+            Name = name;
+            Species = species;
+            Diet = diet;
+            Age = age;
+        }
+
+        public void Roar()
+        {
+            Console.WriteLine($"ROAR! IM {Name}, IM ALSO A {Species}, I EAT {Diet}, AND IM {Age} OLD.s");
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Species: " + Species);
+            Console.WriteLine("Diet: " + Diet);
+            Console.WriteLine("Age: " + Age);
+        }
+    }
+}
---
+++ DinosaurClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinosaurClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dino1 = new Dinosaur("T-Rex", "Tyrannosaurus Rex", "Carnivore", 65);
+            var dino2 = new Dinosaur("Stegosaurus", "Stegosaurus", "Herbivore", 155);
+            var dino3 = new Dinosaur("Velociraptor", "Velociraptor", "Carnivore", 85);  
+
+            dino1.Display();
+            dino1.Roar();
+            dino2.Display();
+            dino2.Roar();
+            dino3.Display();
+            dino3.Roar();
+
+            Console.ReadLine();
+        }
+    }
+}
